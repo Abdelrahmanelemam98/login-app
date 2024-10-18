13f317295031b081364d28e435c91ab32dbafd6b
Abdelrahman Elemam <74329630+Abdelrahmanelemam98@users.noreply.github.com>
2024-10-18 17:42:39 +0300

    add remote componet for details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,29 @@ import { LoginComponent } from './login/login.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { authGuard } from './guard/guards/auth.guard';
 
+// Function to load the remote module
+const loadProductModule = () =>
+  loadRemoteModule({
+    remoteEntry: 'http://localhost:4200/remoteEntry.js',
+    remoteName: 'productApp',
+    exposedModule: './routes',
+  })
+    .then((m) => m.routes)
+    .catch((err) => {
+      console.error('Error loading remote module:', err);
+      throw err;
+    });
+
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
   {
     path: 'product',
-    loadChildren: () =>
-      loadRemoteModule({
-        remoteEntry: 'http://localhost:4200/remoteEntry.js',
-        remoteName: 'productApp',
-        exposedModule: './routes',
-      })
-        .then((m) => m.routes)
-        .catch((err) => {
-          console.error('Error loading remote module:', err);
-          throw err;
-        }),
+    loadChildren: loadProductModule,
     canActivate: [authGuard],
+  },
+  {
+    path: 'product/:id',
+    loadChildren: loadProductModule,
   },
 ];
