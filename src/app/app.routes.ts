d40dd283c77d264e556933de310bf8309d555da0
Abdelrahman Elemam <74329630+Abdelrahmanelemam98@users.noreply.github.com>
2024-10-19 19:18:07 +0300

    update on routing and webpack

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,29 +3,38 @@ import { LoginComponent } from './login/login.component';
 import { loadRemoteModule } from '@angular-architects/module-federation';
 import { authGuard } from './guard/guards/auth.guard';
 
-// Function to load the remote module
-const loadProductModule = () =>
-  loadRemoteModule({
-    remoteEntry: 'http://localhost:4200/remoteEntry.js',
-    remoteName: 'productApp',
-    exposedModule: './routes',
-  })
-    .then((m) => m.routes)
-    .catch((err) => {
-      console.error('Error loading remote module:', err);
-      throw err;
-    });
-
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/login' },
   { path: 'login', component: LoginComponent },
   {
     path: 'product',
-    loadChildren: loadProductModule,
+    loadComponent: () => {
+      return loadRemoteModule({
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        remoteName: 'productApp',
+        exposedModule: './ProductListComponent',
+      })
+        .then((m) => m.ProductListComponent)
+        .catch((err) => {
+          console.error('Error loading remote component:', err);
+          throw err;
+        });
+    },
     canActivate: [authGuard],
   },
   {
     path: 'product/:id',
-    loadChildren: loadProductModule,
+    loadComponent: () => {
+      return loadRemoteModule({
+        remoteEntry: 'http://localhost:4200/remoteEntry.js',
+        remoteName: 'productApp',
+        exposedModule: './ProductDetailsComponent',
+      })
+        .then((m) => m.ProductDetailsComponent)
+        .catch((err) => {
+          console.error('Error loading remote component:', err);
+          throw err;
+        });
+    },
   },
 ];
